4345455eba68fd7c279176c7bd511fbc34bfa33b
renan lima <renangenito@hotmail.com>
2022-01-17 16:43:38 -0300

    Adicionando o banco de dados json server e fazendo a requisição

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 import styles from "../form/Select.module.css";
-function Select({ type, text, name, placeholder, handleOnChange, value }) {
+function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}</label>
-      <select name={name} id={name}>
-      <option>Selecione uma opção</option>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ""}
+      >
+        <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   );
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ProjectForm from '../project/ProjectForm';
-import styles from './NewProjects.module.css'
+import styles from './NewProjects.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function NewProject(){
+
+    const history = useNavigate()
+
+    function createPost(project){
+
+        //initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project),
+        }).then((resp)=> resp.json())
+        .then((data)=> {
+            console.log(data)
+            history('/projects', {message: 'Projeto criado com sucesso!'})
+        }).catch(err => console.log(err))
+    }
+    
     return(
         <div className={styles.newProject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     )
 }
